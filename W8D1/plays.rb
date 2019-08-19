809a7079ccb53cd17c0c1ce3b8a6a95370554da7
Bee <pizzaman8099@gmail.com>
2019-08-18 22:17:44 -0700

    Add non-tested ruby classes

--- W8D1/plays.rb
+++ W8D1/plays.rb
@@ -19,6 +19,23 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        plays.title, plays.year, playwrights.name
+      FROM
+        plays
+      JOIN
+        playwrights ON playwrights.id = plays.playwright_id
+      WHERE
+        title = ?;
+    SQL
+  end
+
+  def self.find_by_playwright(name)
+    Playwright.find_by_name(name)
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -46,6 +63,70 @@ class Play
         title = ?, year = ?, playwright_id = ?
       WHERE
         id = ?
+    SQL
+  end
+
+end
+
+class Playwright
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    raise "#{name} not in database" unless Playwright.all.include?(name)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT 
+        *
+      FROM
+        playwrights
+      JOIN
+        plays ON playwrights.id = plays.playwright_id
+      WHERE
+        plays.name = ?
+    SQL
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name)
+      INSERT INTO
+        playwrights(name)
+      VALUES
+        (?)
+    SQL
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?
+      WHERE
+        id = ?;
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.id)
+      SELECT 
+        title
+      FROM
+        playwrights
+      JOIN
+        plays ON playwrights.id = plays.playwright_id
+      WHERE
+        plays.id = ?
     SQL
   end
 end
