f73ffea7933ef3304fdae9260ebda09005b5285c
Bee <pizzaman8099@gmail.com>
2019-08-20 22:04:33 -0700

    Implement Artist#better_tracks_query

---
+++ W8D3/n+1_buster/app/models/artist.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    # TODO: your code here
+    self.albums
+      .select("albums.title, COUNT(tracks.id) AS tracks_count")
+      .joins(:tracks)
+      .where("albums.artist_id = $1") 
+      .group("tracks.album_id, albums.title").as_json.map(&:compact)
+  end
+end
