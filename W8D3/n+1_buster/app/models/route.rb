a7501a78c5d8148c5eca59f23e8f7b074957c1bc
Bee <pizzaman8099@gmail.com>
2019-08-20 23:22:08 -0700

    Implements route#better_drivers_query

--- W8D3/n+1_buster/app/models/route.rb
+++ W8D3/n+1_buster/app/models/route.rb
@@ -31,5 +31,17 @@ class Route < ApplicationRecord
 
   def better_drivers_query
     # TODO: your code here
+    buses = self.buses.includes(:buses, :drivers)
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
   end
 end
