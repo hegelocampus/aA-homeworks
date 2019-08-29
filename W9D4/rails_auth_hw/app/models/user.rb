b3a80d46d8c0a798ff7b0b97099081c9e11d1f93
Bee <pizzaman8099@gmail.com>
2019-08-29 00:22:59 -0700

    Complete W9D4 HW

--- W9D4/rails_auth_hw/app/models/user.rb
+++ W9D4/rails_auth_hw/app/models/user.rb
@@ -1,2 +1,34 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  include BCrypt
+  before_validation :ensure_session_token
+
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 5, maximum: 15 }, allow_nil: true
+  attr_reader :password
+
+  def self.find_by_credentials(username, pass)
+    user = User.find_by(username: username)
+    user.password_digest == user.pass ? (return user) : (return nil)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(new_password)
+    @password_digest = Password.create(new_password)
+    self.password_digest = @password_digest
+  end
 end
